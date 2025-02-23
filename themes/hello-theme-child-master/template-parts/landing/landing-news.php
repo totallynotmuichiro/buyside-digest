64781c576bcacf19ea1f77d520f46ca777f18930
Muichiro Tokito <sainathrpoojary@gmail.com>
2025-02-23 23:23:56 +0530

    feat(landing): Add new finance images and update article links in landing news section

--- themes/hello-theme-child-master/template-parts/landing/landing-news.php
+++ themes/hello-theme-child-master/template-parts/landing/landing-news.php
@@ -6,9 +6,8 @@ if (empty($articles)) {
     return;
 }
 
-$modal_ids = array_map(function ($index) {
-    return 'article-modal-' . $index;
-}, array_keys($articles));
+$images = [16,17,18,19,20,21,22,23,24,25];
+shuffle($images);
 
 $total_articles = count($articles);
 ?>
@@ -20,14 +19,14 @@ $total_articles = count($articles);
     <div class="mt-6 grid grid-cols-1 md:grid-cols-8 gap-6">
         <!-- Featured Article -->
         <?php if ($total_articles > 0): ?>
-            <div class="col-span-5 md:col-span-3 lg:col-span-4">
+            <div class="col-span-8 md:col-span-3 lg:col-span-4">
                 <div class="overflow-hidden">
-                    <a href="<?php echo $articles[0]['title'] ?>" data-modal-trigger="<?php echo $modal_ids[0]; ?>" class="block">
-                        <img src="<?php echo get_stylesheet_directory_uri() . '/assets/img/finance.jpg' ?>"
+                    <a href="<?php echo $articles[0]['link'] ?>" class="block group" target="_blank">
+                        <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/finance-<?php echo $images[0] ?>.jpg"
                             alt="Article Image"
                             class="w-full h-[350px] object-cover rounded-md">
                         <div class="pt-4">
-                            <h3 class="text-lg font-medium leading-tight mb-2">
+                            <h3 class="text-lg font-medium leading-tight mb-2 group-hover:underline">
                                 <?php echo $articles[0]['title'] ?>
                             </h3>
                             <div class="text-sm text-date space-x-2 mt-1 font-medium">
@@ -49,15 +48,15 @@ $total_articles = count($articles);
 
         <!-- Secondary Articles (2-5) -->
         <?php if ($total_articles > 1): ?>
-            <div class="col-span-5 lg:col-span-4 space-y-6">
+            <div class="col-span-8 lg:col-span-4 space-y-6">
                 <?php for ($i = 1; $i < min(6, $total_articles); $i++): ?>
                     <div class="flex items-center overflow-hidden">
-                        <a href="#" data-modal-trigger="<?php echo $modal_ids[$i]; ?>" class="flex">
-                            <img src="<?php echo get_stylesheet_directory_uri() . '/assets/img/finance.jpg' ?>"
+                        <a href="<?php echo $articles[$i]['link'] ?>" class="flex group" target="_blank">
+                            <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/finance-<?php echo $images[$i] ?>.jpg"
                                 alt="<?php echo $articles[$i]['title']; ?>"
                                 class="w-28 h-16 object-cover rounded-md">
                             <div class="pl-5">
-                                <h3 class="leading-tight font-medium text-lg line-clamp-1">
+                                <h3 class="leading-tight font-medium text-lg line-clamp-1 group-hover:underline">
                                     <?php echo $articles[$i]['title']; ?>
                                 </h3>
                                 <div class="text-sm text-date space-x-2 mt-1 font-medium">
@@ -81,12 +80,12 @@ $total_articles = count($articles);
         <!-- Additional Articles (6-9) -->
         <?php if ($total_articles > 6): ?>
             <?php for ($i = 6; $i < min(10, $total_articles); $i++): ?>
-                <a class="flex overflow-hidden col-span-8 lg:col-span-4">
-                    <img src="<?php echo get_stylesheet_directory_uri() . '/assets/img/finance.jpg' ?>"
+                <a href="<?php echo $articles[$i]['link'] ?>" class="flex overflow-hidden col-span-8 lg:col-span-4 group" target="_blank">
+                    <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/finance-<?php echo $images[$i] ?>.jpg"
                         alt="<?php echo $articles[$i]['title']; ?>"
                         class="h-16 object-cover rounded-md">
                     <div class="pl-5">
-                        <h3 class="leading-tight font-medium text-lg line-clamp-1">
+                        <h3 class="leading-tight font-medium text-lg line-clamp-1 group-hover:underline">
                             <?php echo $articles[$i]['title']; ?>
                         </h3>
                         <div class="text-sm text-date space-x-2 mt-1 font-medium">
