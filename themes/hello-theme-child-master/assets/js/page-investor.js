d01720064a93b9f22b92fde9eac3e645f221b057
Muichiro Tokito <sainathrpoojary@gmail.com>
2025-02-27 23:03:15 +0530

    feat(investor): Add try catch n treemap init

--- themes/hello-theme-child-master/assets/js/page-investor.js
+++ themes/hello-theme-child-master/assets/js/page-investor.js
@@ -1,171 +1,192 @@
 document.addEventListener("DOMContentLoaded", async function () {
-  // Get the investor name from the URL
-  const url = window.location.href;
-  const cleanUrl = url.split("?")[0].replace(/\/$/, "");
-  let investorName = cleanUrl.split("/").pop();
+  try {
+    // Get the investor name from the URL
+    const url = window.location.href;
+    const cleanUrl = url.split("?")[0].replace(/\/$/, "");
+    let investorName = cleanUrl.split("/").pop();
 
-  // Convert dashes to spaces
-  investorName = investorName.replace(/-/g, " ");
+    // Convert dashes to spaces
+    investorName = investorName.replace(/-/g, " ");
 
-  // Fetch the data
-  const response = await fetch(`/wp-content/uploads/data.json`);
-  if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`);
-  }
-  const data = await response.json();
-
-  // Log the data
-  let holdings = data[0].holdings;
-
-  // Total value of the portfolio
-  const totalValue = holdings.reduce((acc, holding) => {
-    return acc + holding.value;
-  }, 0);
-
-  // Keep only value, industry, company_name, share_change_pct, market_cap_millions
-  holdings = holdings.map((holding) => {
-    return {
-      value: holding.value,
-      industry: holding.industry,
-      companyName: holding.company_name,
-      ticker: holding.ticker,
-      sharesChange: holding.shares_change_pct,
-      marketCap: holding.market_cap_millions,
-      percentageOfPortfolio: (holding.value / totalValue) * 100,
-    };
-  });
-
-  // Group by industry
-  const holdingsGroupedByIndustry = holdings.reduce((acc, holding) => {
-    const industry = holding.industry;
-    if (!acc[industry]) {
-      acc[industry] = [];
+    // Fetch the data
+    const response = await fetch(`/wp-content/uploads/data.json`);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
-    acc[industry].push(holding);
-    return acc;
-  }, {});
+    const data = await response.json();
 
-  // Function to get color as per share change
-  const getColorForChange = (sharesChange) => {
-    const maxChange = 5;
-    const saturation = Math.min(Math.abs(sharesChange) / maxChange, 1);
-    const alpha = 0.3 + saturation * 0.7;
-    return sharesChange > 0
-      ? `rgba(77, 175, 74, ${alpha})`
-      : `rgba(228, 26, 28, ${alpha})`;
-  };
+    let holdings = data[0].holdings;
 
-  // Convert the data to the format required by echarts
-  const chartData = {
-    name: "Financial Services",
-    children: [],
-  };
-
-  for (const industry in holdingsGroupedByIndustry) {
-    const industryHoldings = holdingsGroupedByIndustry[industry];
-
-    // Calculate the change for the industry
-    const industryChange = industryHoldings.reduce((acc, holding) => {
-      return acc + holding.sharesChange;
+    // Total value of the portfolio
+    const totalValue = holdings.reduce((acc, holding) => {
+      return acc + holding.value;
     }, 0);
 
-    // Create the industry item
-    const industryItem = {
-      name: industry,
-      children: [],
-      change: industryChange / industryHoldings.length,
-      itemStyle: {
-        color: getColorForChange(industryChange),
-        borderColor: getColorForChange(industryChange),
-      },
+    // Keep only value, industry, company_name, share_change_pct, market_cap_millions
+    holdings = holdings.map((holding) => {
+      return {
+        value: holding.value,
+        industry: holding.industry,
+        companyName: holding.company_name,
+        ticker: holding.ticker,
+        sharesChange: holding.shares_change_pct,
+        marketCap: holding.market_cap_millions,
+        percentageOfPortfolio: (holding.value / totalValue) * 100,
+      };
+    });
+
+    // Group by industry
+    const holdingsGroupedByIndustry = holdings.reduce((acc, holding) => {
+      const industry = holding.industry;
+      if (!acc[industry]) {
+        acc[industry] = [];
+      }
+      acc[industry].push(holding);
+      return acc;
+    }, {});
+
+    // Function to get color as per share change
+    const getColorForChange = (sharesChange) => {
+      const maxChange = 5;
+      const saturation = Math.min(Math.abs(sharesChange) / maxChange, 1);
+      const alpha = 0.3 + saturation * 0.7;
+      return sharesChange > 0
+        ? `rgba(77, 175, 74, ${alpha})`
+        : `rgba(228, 26, 28, ${alpha})`;
     };
 
-    // Add the companies
-    for (const holding of industryHoldings) {
-      const companyItem = {
-        name: holding.ticker,
-        value: holding.percentageOfPortfolio,
-        change: holding.sharesChange,
+    // Convert the data to the format required by echarts
+    const chartData = {
+      name: "Financial Services",
+      children: [],
+    };
+
+    for (const industry in holdingsGroupedByIndustry) {
+      const industryHoldings = holdingsGroupedByIndustry[industry];
+
+      // Calculate the change for the industry
+      const industryChange = industryHoldings.reduce((acc, holding) => {
+        return acc + holding.sharesChange;
+      }, 0);
+
+      // Create the industry item
+      const industryItem = {
+        name: industry,
+        children: [],
+        change: industryChange / industryHoldings.length,
         itemStyle: {
-          color: getColorForChange(holding.sharesChange),
-          borderColor: getColorForChange(holding.sharesChange),
+          color: getColorForChange(industryChange),
+          borderColor: getColorForChange(industryChange),
         },
       };
-      industryItem.children.push(companyItem);
+
+      // Add the companies
+      for (const holding of industryHoldings) {
+        const companyItem = {
+          name: holding.ticker,
+          value: holding.percentageOfPortfolio,
+          change: holding.sharesChange,
+          itemStyle: {
+            color: getColorForChange(holding.sharesChange),
+            borderColor: getColorForChange(holding.sharesChange),
+          },
+        };
+        industryItem.children.push(companyItem);
+      }
+
+      chartData.children.push(industryItem);
     }
 
-    chartData.children.push(industryItem);
-  }
-
-  // Chart config
-  const chartConfig = {
-    tooltip: {
-      formatter: (info) => {
-        const value = info.value ? `${info.value.toFixed(2)}%` : "";
-        const change =
-          info.data.change !== undefined
-            ? `${info.data.change.toFixed(2)}%`
-            : "N/A";
-        return `${info.name}<br>Value: ${value}<br>Change: ${change}`;
+    // Chart config
+    const chartConfig = {
+      title: {
+        text: `${investorName
+          .charAt(0)
+          .toUpperCase()
+          .concat(investorName.slice(1))}'s Holdings Heat Map`,
+        left: "left",
+        top: "top",
+        textStyle: {
+          color: "#000",
+          fontSize: 18,
+          fontWeight: 500,
+        },
       },
-    },
-    series: [
-      {
-        type: "treemap",
-        data: [chartData],
-        roam: false,
-        breadcrumb: { show: true },
-        label: { show: true, formatter: "{b}",  overflow: "truncate" },
-        upperLabel: { show: true, height: 30 },
-        itemStyle: { borderWidth: 1, gapWidth: 2 },
-        levels: [
-          {},
-          {
-            itemStyle: { borderWidth: 5, gapWidth: 10, borderColor: "#000000" },
-            upperLabel: {
-              show: true,
-              textBorderWidth: 0,
-              textBorderColor: "transparent",
-              color: "#fff",
-            },
-          },
-          {
-            upperLabel: {
-              show: true,
-              textBorderWidth: 0,
-              textBorderColor: "transparent",
-              color: "#fff",
-            },
-            itemStyle: { borderWidth: 5, gapWidth: 1 },
-          },
-          {
-            label: {
-              formatter: (info) => {
-                return [`{a|${info.name}}`, `{b|${info.data.change.toFixed(2)}%}`].join(
-                  "\n"
-                );
+      tooltip: {
+        formatter: (info) => {
+          const value = info.value ? `${info.value.toFixed(2)}%` : "";
+          const change =
+            info.data.change !== undefined
+              ? `${info.data.change.toFixed(2)}%`
+              : "N/A";
+          return `${info.name}<br>Value: ${value}<br>Change: ${change}`;
+        },
+      },
+      series: [
+        {
+          type: "treemap",
+          data: [chartData],
+          roam: true,
+          breadcrumb: { show: true },
+          label: { show: true, formatter: "{b}", overflow: "truncate" },
+          upperLabel: { show: true, height: 30 },
+          itemStyle: { borderWidth: 1, gapWidth: 2 },
+          levels: [
+            {},
+            {
+              itemStyle: {
+                borderWidth: 5,
+                gapWidth: 10,
+                borderColor: "#000000",
               },
-              rich: {
-                a: {
-                  color: "#fff",
-                  fontSize: 25,
-                  fontWeight: 500,
-                  padding: [0, 0, 5, 0],
+              upperLabel: {
+                show: true,
+                textBorderWidth: 0,
+                textBorderColor: "transparent",
+                color: "#fff",
+              },
+            },
+            {
+              upperLabel: {
+                show: true,
+                textBorderWidth: 0,
+                textBorderColor: "transparent",
+                color: "#fff",
+              },
+              itemStyle: { borderWidth: 5, gapWidth: 1 },
+            },
+            {
+              label: {
+                formatter: (info) => {
+                  return [
+                    `{a|${info.name}}`,
+                    `{b|${info.data.change.toFixed(2)}%}`,
+                  ].join("\n");
                 },
-                b: { color: "#fff", fontSize:15, fontWeight: 300 },
+                rich: {
+                  a: {
+                    color: "#fff",
+                    fontSize: 25,
+                    fontWeight: 500,
+                    padding: [0, 0, 5, 0],
+                  },
+                  b: { color: "#fff", fontSize: 15, fontWeight: 300 },
+                },
+                padding: 10,
               },
-              padding: 10,
             },
-          },
-        ],
-      },
-    ],
-  };
+          ],
+        },
+      ],
+    };
 
-  // Get and render the chart
-  const chartContainer = document.getElementById("main");
-  const treeMapChart = echarts.init(chartContainer);
-  treeMapChart.resize();
-  treeMapChart.setOption(chartConfig);
-  window.addEventListener("resize", () => treeMapChart.resize());
+    // Get and render the chart
+    const chartContainer = document.getElementById("main");
+    const treeMapChart = echarts.init(chartContainer);
+    treeMapChart.resize();
+    treeMapChart.setOption(chartConfig);
+    window.addEventListener("resize", () => treeMapChart.resize());
+  } catch (error) {
+    console.error("Error in chart initialization:", error);
+  }
 });
--- themes/hello-theme-child-master/template-parts/investor/investor-treemap.php
+++ themes/hello-theme-child-master/template-parts/investor/investor-treemap.php
@@ -1,4 +1,3 @@
 <div class="lg:col-span-6 border text-card-foreground w-full mx-auto bg-white shadow-md rounded-xl overflow-hidden p-4">
-    <h1>Arnold Van Den Berg's Holdings Heat Map</h1>
     <div id="main" style="position: relative; width: 100%; height: 400px;"></div>
 </div>
